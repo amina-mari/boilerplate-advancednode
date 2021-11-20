e27dd80ebb2a9723021d831883315febf27b2728
Mariana <mariana.dantasweb7@gmail.com>
2021-11-20 14:41:09 -0300

    Add passport and expresss-session packages, configure sesssion in express and set the session secret in .env file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 'use strict';
-require('dotenv').config();
+const path = require('path')
+require('dotenv').config({
+  path: path.join(__dirname, 'sample.env')
+});
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -11,6 +16,14 @@ app.set('view engine', 'pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {secure: false}
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/').get((req, res) => {
   res.render(__dirname + '/views/pug/index', {title: 'Hello', message: 'Please login'});
